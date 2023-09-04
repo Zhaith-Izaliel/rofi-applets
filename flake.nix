fce26b576968948035b72c331029c5c4efccfef9
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2023-09-04 18:22:11 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = inputs: with inputs;
+  flake-utils.lib.eachDefaultSystem (system:
+    with import nixpkgs { inherit system; };
+    rec {
+      workspaceShell = pkgs.mkShell {
+        # nativeBuildInputs is usually what you want -- tools you need to run
+        nativeBuildInputs = with pkgs; [
+          rofi
+          pkexec
+          acpi
+          powertop
+          brightnessctl
+          mpc
+          mpd
+          firefox
+          wireplumber
+          pavucontrol
+        ];
+      };
+
+      devShells = {
+        # nix develop
+        "${system}".default = workspaceShell;
+        default = workspaceShell;
+      };
+      packages = {
+        shell = devShells.default;
+      };
+    }
+  );
+}
+
